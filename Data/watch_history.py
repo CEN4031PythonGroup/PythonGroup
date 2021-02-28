a05170e1ae03ecf1176a95437021c1b9413c0971
jwilc <45748957+jwilc@users.noreply.github.com>
2021-02-28 14:48:24 -0500

    Add files via upload
    Updating contents of Data directory

--- Data/watch_history.py
+++ Data/watch_history.py
@@ -1,14 +1,19 @@
 # coding=utf-8
 # Class representation of the database structure. Table stores the time lapsed for each run of the stopwatch.
-from sqlalchemy import Column, String, Integer, Date
+from sqlalchemy import Column, Float, Integer
 from base import Base
 
 
 class WatchHistory(Base):
     __tablename__ = 'WatchHistory'
 
-    sessionID = Column(Integer, primary_key=True)
-    timeLapsed = Column(Integer)
+    sessionID = Column(Integer, primary_key=True)  # Should auto increment
+    timeLapsed = Column(Float)
 
     def __init__(self, time_lapsed):
         self.timeLapsed = time_lapsed
+
+    def __str__(self):
+        return f'WatchHistory({self.sessionID},{self.timeLapsed})'
+
+
